6e33e5daa72ea970f3d90c210df631800862c3bd
WeslleyLuccas <132603406+WeslleyLuccas@users.noreply.github.com>
2024-08-09 10:23:21 -0300

    Add files via upload

---
+++ C# Linguagem Desktop/18.03 lista/18.03 lista 1 quest 3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18._03_lista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero para saber se e impar ou par!");
+            int numero = int.Parse(Console.ReadLine());
+            int divisao = numero % 2;
+
+            if (divisao == 0)
+            {
+                Console.WriteLine("O seu numero (" + numero + ") e par!");
+            }
+            else
+            {
+                Console.WriteLine("O seu numero (" + numero + ") e impar!");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ C# Linguagem Desktop/Lista de Exercicio/Lista de Exercicio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_de_Exercicio
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Qual sua idade? ");
+            int idade = int.Parse(Console.ReadLine());
+
+            int maioridade = 18;
+
+            if (idade >= maioridade)
+            {
+                Console.WriteLine("Voce e maior de idade!");
+            }
+            else
+            {
+                Console.WriteLine("Voce e de menor idade!");
+            }
+        }
+    }
+}
